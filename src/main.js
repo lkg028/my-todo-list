d53b2fcf7a4ff8809c14441da74d757b0e10c09a
lkg028 <lkg028@outlook.com>
2019-10-02 00:19:35 +0800

    初始化vue项目、安装依赖、安装css预处理器、引入基本css样式，fastclick

---
+++ src/main.js
@@ -0,0 +1,20 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+// 引入基本CSS样式：reset.css,border.css
+import 'styles/reset.css'
+import 'styles/border.css'
+// 引入fastclick解决移动端300毫秒点击延迟事件
+import fastclick from 'fastclick'
+fastclick.attach(document.body)
+Vue.config.productionTip = false
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="container">
+    这个是页面的首页
+  </div>
+</template>
+<script>
+export default {
+}
+</script>
+<style lang="stylus" scoped>
+
+</style>
